5558488335b8a295ce6c0247b1d2a6efde2fb9dc
CRISTHIAN VILLEGAS GARCIA <cristianvg9692@gmail.com>
2022-04-22 14:16:49 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'f_agenda';
+export class AppComponent implements OnInit {
+  public title = 'Agenda';
+  public tasks: any = [];
+
+  public activity: string = "";
+  public date: string = "";
+  public time: string = "";
+
+  constructor(private http: HttpClient, private toastr: ToastrService){
+  }
+
+  ngOnInit(): void {
+      this.getTasks();
+  }
+
+
+  public addTask(): void {
+    const data = {
+      activity: this.activity,
+      date: this.date,
+      estimated: this.time
+    };
+    this.http.post("http://localhost:8080/task", data).subscribe(
+      resp => {
+        this.toastr.success('', JSON.stringify(resp));
+        this.getTasks();
+      },
+      err => { console.log(err) }
+    );
+  }
+
+  public getTasks(): void {
+    this.http.get("http://localhost:8080/task").subscribe(
+      success => {
+        this.tasks = success;
+      },
+      err => { console.log(err) }
+    );
+  }
+
+  public filteredSearch(filter: string): void {
+    this.http.get("http://localhost:8080/task/"+filter).subscribe(
+      success => {
+        this.tasks = success;
+      },
+      err => { console.log(err) }
+    );
+  }
+
+  public deleteTask(id: number): void {
+    
+    if (confirm("DESEAS ELIMINAR ESTE REGISTRO?")) {
+      this.http.delete("http://localhost:8080/task/"+id).subscribe(
+      success => {
+        alert(success);
+        this.getTasks();
+      },
+      err => { console.log(err) }
+    );
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 import { AppComponent } from './app.component';
+
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
